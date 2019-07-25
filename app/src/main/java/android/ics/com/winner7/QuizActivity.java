47dae169c52d0d56880dd030d35f7baccadcd567
NothingbutPro <paragsharma611@gmail.com>
2019-07-25 15:10:36 +0530

    pro2

--- app/src/main/java/android/ics/com/winner7/QuizActivity.java
+++ app/src/main/java/android/ics/com/winner7/QuizActivity.java
@@ -1,10 +1,12 @@
 package android.ics.com.winner7;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.ics.com.winner7.Adapter.TestAdapter;
 import android.ics.com.winner7.Model.TestModel;
 import android.ics.com.winner7.Parsing_Quiz.Quiz;
+import android.ics.com.winner7.Utils.AppPreference;
 import android.ics.com.winner7.Utils.Connectivity;
 import android.ics.com.winner7.Utils.HttpHandler;
 import android.os.AsyncTask;
@@ -24,14 +26,32 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class QuizActivity extends AppCompatActivity {
     String server_url;
    static int  counter=0;
+   TextView sumtxt;
    TextView tv_quiz_prev,tv_quiz_next;
    TextView questxt;
+    int rightcount;
+    int attempted;
+    int unattempted;
+    int wrongcount;
     TextView ttxp;
+    int totalmarks;
     int time = 10;
     RelativeLayout rl_ans1,rl_ans2,rl_ans3,rl_ans4;
     TextView tv_quiz_subject, tv_quiz_ans1, tv_quiz_ans2, tv_quiz_ans3, tv_quiz_ans4, tv_quiz_ans5, tv_quiz_ans6;
@@ -53,6 +73,7 @@ public class QuizActivity extends AppCompatActivity {
         tv_quiz_subject = (TextView) findViewById(R.id.tv_quiz_subject);
         questxt = (TextView) findViewById(R.id.questxt);
         ttxp = (TextView) findViewById(R.id.ttxp);
+        sumtxt = (TextView) findViewById(R.id.sumtxt);
         tv_quiz_ans1 = (TextView) findViewById(R.id.tv_quiz_ans1);
         tv_quiz_ans2 = (TextView) findViewById(R.id.tv_quiz_ans2);
         tv_quiz_ans3 = (TextView) findViewById(R.id.tv_quiz_ans3);
@@ -66,7 +87,31 @@ public class QuizActivity extends AppCompatActivity {
         rl_ans2 = (RelativeLayout)findViewById(R.id.rl_ans2);
         rl_ans3 = (RelativeLayout)findViewById(R.id.rl_ans3);
         rl_ans4 = (RelativeLayout)findViewById(R.id.rl_ans4);
+        sumtxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0;i<quizLinkedList.size();i++) {
+                    String ansss = quizLinkedList.get(i).getAns();
+                    String myansx = quizLinkedList.get(i).getMyeans();
+                    if(quizLinkedList.get(i).getAns().equals(quizLinkedList.get(i).getMyeans()))
+                    {
+                        totalmarks = Integer.valueOf(quizLinkedList.get(i).getMarks()) + totalmarks;
+                        rightcount++;
+                        attempted++;
+                    }else {
+                        if(quizLinkedList.get(i).getMyeans().equals("200"))
+                        {
+                            unattempted++;
+                        }else {
+                            attempted++;
+                            wrongcount++;
+                        }
+                    }
+                }
 
+                new AddResult(quizLinkedList.get(0).getId() ,"5",totalmarks ,"7/24/2019" ,"4:09" ,attempted,unattempted,rightcount,wrongcount).execute();
+            }
+        });
         new CountDownTimer(10000, 1000) {
 
             public void onTick(long millisUntilFinished) {
@@ -90,7 +135,7 @@ public class QuizActivity extends AppCompatActivity {
                 rl_ans3.setBackgroundColor(Color.GRAY);
                 rl_ans4.setBackgroundColor(Color.GRAY);
                 myans1 = "A";
-                myans = "A";
+                myans = "1";
                 myans2 = "X";
                 myans3 = "X";
                 myans4 = "X";
@@ -103,7 +148,7 @@ public class QuizActivity extends AppCompatActivity {
                 rl_ans3.setBackgroundColor(Color.GRAY);
                 rl_ans4.setBackgroundColor(Color.GRAY);
                 myans2 = "B";
-                myans = "B";
+                myans = "2";
                 myans1 = "X";
                 myans3 = "X";
                 myans4 = "X";
@@ -118,7 +163,7 @@ public class QuizActivity extends AppCompatActivity {
                 rl_ans4.setBackgroundColor(Color.GRAY);
                // rl_ans3.setBackgroundColor(Color.GRAY);
                 myans3 = "C";
-                myans = "C";
+                myans = "3";
                 myans1 = "X";
                 myans2 = "X";
                 myans4 = "X";
@@ -131,7 +176,7 @@ public class QuizActivity extends AppCompatActivity {
                 rl_ans2.setBackgroundColor(Color.GRAY);
                 rl_ans3.setBackgroundColor(Color.GRAY);
                 myans4 = "D";
-                myans = "D";
+                myans = "4";
                 myans1 = "X";
                 myans2 = "X";
                 myans3 = "X";
@@ -144,12 +189,17 @@ public class QuizActivity extends AppCompatActivity {
                 Log.e("counterwa" , ""+counter);
               //  quizLinkedList.add(new Quiz(id,Que,Ans_1,Ans_2,Ans_3 ,Ans_4,Ans,marks,Createdate,status ,myans));
                 try {
-                    quizLinkedList.get(counter).setMyeans(myans);
+                    if(myans.length() !=0) {
+                        quizLinkedList.get(counter).setMyeans(myans);
+                    }else {
+                        quizLinkedList.get(counter).setMyeans("200");
+                    }
                     try {
                         String mynextans = quizLinkedList.get(1 + counter).getMyeans().toString();
                         if (quizLinkedList.get(1 + counter).getMyeans().toString().length() != 0) {
-                            if(mynextans.equals("A"))
+                            if(mynextans.equals("1"))
                             {
+//                                if(quizLinkedList.get(counter).)
                                 counter++;
                                 rl_ans1.setBackgroundColor(Color.GREEN);
                                 //    rl_ans4.setBackgroundColor(Color.GRAY);
@@ -162,7 +212,10 @@ public class QuizActivity extends AppCompatActivity {
                                 tv_quiz_ans2.setText(quizLinkedList.get(counter).getAns2());
                                 tv_quiz_ans3.setText(quizLinkedList.get(counter).getAns3());
                                 tv_quiz_ans4.setText(quizLinkedList.get(counter).getAns4());
-                            }else if(mynextans.equals("B"))
+//                                new AddResult(quizLinkedList.get(counter-1).getId() ,quizLinkedList.size() ,
+//                                        quizLinkedList.get(counter-1).getMarks() ,quizLinkedList.get(counter-1).get ,
+//                                        quizLinkedList.get(counter-1).getId() ,quizLinkedList.get(counter-1).getId() ,).execute();
+                            }else if(mynextans.equals("2"))
                             {
                                 counter++;
                                 rl_ans2.setBackgroundColor(Color.GREEN);
@@ -175,7 +228,7 @@ public class QuizActivity extends AppCompatActivity {
                                 tv_quiz_ans2.setText(quizLinkedList.get(counter).getAns2());
                                 tv_quiz_ans3.setText(quizLinkedList.get(counter).getAns3());
                                 tv_quiz_ans4.setText(quizLinkedList.get(counter).getAns4());
-                            }else if(mynextans.equals("C"))
+                            }else if(mynextans.equals("3"))
                             {
                                 counter++;
                                 rl_ans3.setBackgroundColor(Color.GREEN);
@@ -188,7 +241,7 @@ public class QuizActivity extends AppCompatActivity {
                                 tv_quiz_ans2.setText(quizLinkedList.get(counter).getAns2());
                                 tv_quiz_ans3.setText(quizLinkedList.get(counter).getAns3());
                                 tv_quiz_ans4.setText(quizLinkedList.get(counter).getAns4());
-                            }else if(mynextans.equals("D"))
+                            }else if(mynextans.equals("4"))
                             {
                                 counter++;
                                 rl_ans4.setBackgroundColor(Color.GRAY);
@@ -266,6 +319,12 @@ public class QuizActivity extends AppCompatActivity {
                     }catch (Exception Ex)
                     {
                         Ex.printStackTrace();
+                        sumtxt.setVisibility(View.VISIBLE);
+                        if(myans.length() ==0) {
+                            quizLinkedList.get(counter-1).setMyeans(myans);
+                        }else {
+                            quizLinkedList.get(counter-1).setMyeans("200");
+                        }
 //                        counter =0;
                         Toast.makeText(QuizActivity.this, "THat was last question", Toast.LENGTH_SHORT).show();
                     }
@@ -285,7 +344,7 @@ public class QuizActivity extends AppCompatActivity {
                     int coun3 = counter-1;
                     String mynextans = quizLinkedList.get(coun3).getMyeans().toString();
                     if (quizLinkedList.get(coun3).getMyeans().toString().length() != 0) {
-                        if(mynextans.equals("A"))
+                        if(mynextans.equals("1"))
                         {
                             if(counter !=0) {
                                 counter--;
@@ -307,7 +366,7 @@ public class QuizActivity extends AppCompatActivity {
 //                                counter =0;
                                 Toast.makeText(QuizActivity.this, "you reachead first", Toast.LENGTH_SHORT).show();
                             }
-                        }else if(mynextans.equals("B"))
+                        }else if(mynextans.equals("2"))
                         {
                             if(counter !=0) {
                                 counter--;
@@ -325,7 +384,7 @@ public class QuizActivity extends AppCompatActivity {
                             }else {
                                 Toast.makeText(QuizActivity.this, "You are at first", Toast.LENGTH_SHORT).show();
                             }
-                        }else if(mynextans.equals("C"))
+                        }else if(mynextans.equals("3"))
                         {
                             if(counter !=0) {
                                 counter--;
@@ -344,7 +403,7 @@ public class QuizActivity extends AppCompatActivity {
                             }else {
                                 Toast.makeText(QuizActivity.this, "fisrt", Toast.LENGTH_SHORT).show();
                             }
-                        }else if(mynextans.equals("D"))
+                        }else if(mynextans.equals("4"))
                         {
                             if(counter !=0) {
                                 counter--;
@@ -495,5 +554,168 @@ public class QuizActivity extends AppCompatActivity {
                 super.onPostExecute(output);
             }
         }
+    }
+
+    private class AddResult extends AsyncTask<String, String, String> {
+
+        ProgressDialog dialog;
+        String id ,ques,date,datetime;
+        int totalmarks,attempted,rightcount,wrongcount,unattempted ;
+
+        public AddResult(String id, String s, int totalmarks, String s1, String s2, int attempted, int unattempted, int rightcount, int wrongcount) {
+            this.id = id;
+            this.ques = s;
+            this.totalmarks = totalmarks;
+            this.date = s1;
+            this.datetime = s2;
+            this.attempted = attempted;
+            this.unattempted = unattempted;
+            this.rightcount = rightcount;
+            this.wrongcount = wrongcount;
+
+        }
+
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(QuizActivity.this);
+            dialog.show();
+
+        }
+
+        protected String doInBackground(String... arg0) {
+
+            try {
+
+                URL url = new URL("http://ihisaab.in/winnerseven/api/addResult");
+
+
+                JSONObject postDataParams = new JSONObject();
+
+                postDataParams.put("user_id", AppPreference.getId(QuizActivity.this));
+                postDataParams.put("totalquestuion", ques);
+                postDataParams.put("totalmark", quizLinkedList.get(0).getMarks());
+                postDataParams.put("gainmark", totalmarks);
+                postDataParams.put("date", date);
+                postDataParams.put("datetime", datetime);
+                postDataParams.put("attempted", attempted);
+                postDataParams.put("unattempted", unattempted);
+                postDataParams.put("rigth", rightcount);
+                postDataParams.put("wrong", wrongcount);
+
+
+                Log.e("postDataParams", postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds*/);
+                conn.setConnectTimeout(15000  /*milliseconds*/);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    /*BufferedReader in = new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line = "";
+
+                    while ((line = in.readLine()) != null) {
+
+                        StringBuffer Ss = sb.append(line);
+                        Log.e("Ss", Ss.toString());
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString(); */
+
+                    BufferedReader r = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        result.append(line);
+                    }
+                    r.close();
+                    return result.toString();
+
+                } else {
+                    return new String("false : " + responseCode);
+                }
+            } catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                dialog.dismiss();
+
+                // JSONObject jsonObject = null;
+                Log.e("SendJsonDataToServer>>>", result.toString());
+                try {
+
+                    JSONObject jsonObject = new JSONObject(result);
+                    String responce = jsonObject.getString("responce");
+                    if(responce.equals("true"))
+                    {
+                        Intent intent = new Intent(QuizActivity.this , QuizResultActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+//                    JSONObject object = jsonObject.getJSONObject("massage");
+//                    if(object.getString("id").length() !=0)
+//                    {
+//                        Toast.makeText(QuizActivity.this, "Test Success", Toast.LENGTH_SHORT).show();
+//                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+        public String getPostDataString(JSONObject params) throws Exception {
+
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            Iterator<String> itr = params.keys();
+
+            while (itr.hasNext()) {
+
+                String key = itr.next();
+                Object value = params.get(key);
+
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(key, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+            }
+            return result.toString();
+        }
+
+
     }
 }
---
+++ app/src/main/java/android/ics/com/winner7/QuizResultActivity.java
@@ -0,0 +1,189 @@
+package android.ics.com.winner7;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.ics.com.winner7.Utils.AppPreference;
+import android.ics.com.winner7.Utils.SessionManager;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class QuizResultActivity extends AppCompatActivity {
+    TextView attemp,unattepmt,totmark,timewa,wrontxt,gainmark,rigth;
+    SessionManager sessionManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_result);
+        attemp = findViewById(R.id.attemp);
+        gainmark = findViewById(R.id.gainmark);
+        rigth = findViewById(R.id.rigth);
+        unattepmt = findViewById(R.id.unattepmt);
+        totmark = findViewById(R.id.totmark);
+        timewa = findViewById(R.id.timewa);
+        wrontxt = findViewById(R.id.wrontxt);
+        sessionManager = new SessionManager(this);
+        Log.e("add pre" , ""+AppPreference.getId(this));
+        new GETALlRESULTS(AppPreference.getId(this)).execute();
+    }
+
+    private class GETALlRESULTS  extends AsyncTask<String, String, String> {
+
+        ProgressDialog dialog;
+        String u_id;
+
+        public GETALlRESULTS(String id) {
+            this.u_id = id;
+        }
+
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(QuizResultActivity.this);
+            dialog.show();
+
+        }
+
+        protected String doInBackground(String... arg0) {
+
+            try {
+
+                URL url = new URL("http://ihisaab.in/winnerseven/api/getresultbyuser");
+
+
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("user_id", u_id);
+
+
+                Log.e("postDataParams", postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds*/);
+                conn.setConnectTimeout(15000  /*milliseconds*/);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    /*BufferedReader in = new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line = "";
+
+                    while ((line = in.readLine()) != null) {
+
+                        StringBuffer Ss = sb.append(line);
+                        Log.e("Ss", Ss.toString());
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString(); */
+
+                    BufferedReader r = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        result.append(line);
+                    }
+                    r.close();
+                    return result.toString();
+
+                } else {
+                    return new String("false : " + responseCode);
+                }
+            } catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                dialog.dismiss();
+
+                // JSONObject jsonObject = null;
+                Log.e("SendJsonDataToServer>>>", result.toString());
+                try {
+
+                    JSONObject jsonObject = new JSONObject(result);
+                    String responce = jsonObject.getString("responce");
+
+                    if(responce.equals("true"))
+                    {
+                        JSONObject object = jsonObject.getJSONObject("massage");
+                        totmark.setText(object.get("totalmark").toString());
+                        attemp.setText(object.get("attempted").toString());
+                        unattepmt.setText(object.get("unattempted").toString());
+                        rigth.setText(object.get("rigth").toString());
+                        gainmark.setText(object.get("gainmark").toString());
+                        wrontxt.setText(object.get("wrong").toString());
+                        timewa.setText(object.get("datetime").toString());
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+        public String getPostDataString(JSONObject params) throws Exception {
+
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            Iterator<String> itr = params.keys();
+
+            while (itr.hasNext()) {
+
+                String key = itr.next();
+                Object value = params.get(key);
+
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(key, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+            }
+            return result.toString();
+        }
+
+
+    }
+}
